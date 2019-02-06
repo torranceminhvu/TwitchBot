60c90c60e93d770608cac8cd1a20c6c6ff6272a5
Minh Vu <torranceminhvu@yahoo.com>
2019-02-05 23:01:22 -0800

    Building first bot

---
+++ bot.js
@@ -0,0 +1,73 @@
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: "aquasniper1",
+    password: "oauth:wtpujk4qyccrecjnk84doesnb9hdgz"
+  },
+  channels: [
+    "aquasniper1"
+  ]
+};
+
+const allowedUser = [
+  "aquasniper1"
+];
+
+const chatCommand = 'POGGERS';
+const botDelayInMS = 400;
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('chat', onChatHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+// Called every time a message comes in
+async function onChatHandler(channel, userstate, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+  console.log(userstate);
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === chatCommand && allowedUser.includes(userstate.username)) {
+    const botMessages = buildPyramid();
+
+    for (let botMessage of botMessages) {
+      client.say(channel, `${botMessage}`);
+      await sleep(botDelayInMS);
+    };
+
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
+
+function buildPyramid() {
+  const messages = [
+    'oddonePat oddonePat oddonePat oddonePat',
+    'oddonePat oddoneG oddoneBakana oddonePat',
+    'oddonePat oddoneLewd oddoneLOL oddonePat',
+    'oddonePat oddonePat oddonePat oddonePat'
+  ];
+
+  return messages;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
